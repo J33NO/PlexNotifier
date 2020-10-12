b10af15a541cc62766bfe9d5981d556a1d9b0d6f
Daniel Carlson <performancerepair@gmail.com>
2020-10-11 21:52:22 -0700

    Swap to using Entity Framework for CRUD of user data.

---
+++ DAL/UserContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plex_Notifier_App
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base("UserContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
